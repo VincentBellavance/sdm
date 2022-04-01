ca7f0eb5a159f1360034b9114574e55fde630605
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-04-01 11:55:15 -0400

    fix filter_abs

--- R/filter_abs.R
+++ R/filter_abs.R
@@ -8,10 +8,10 @@ filter_abs <- function(obs, perc) {
 
   abs <- obs[obs$occurrence == 0,]
 
-  abs <- abs(sample(nrow(abs), nrow(abs)*perc))
+  abs <- abs[sample(nrow(abs), nrow(abs)*perc),]
 
   obs <- rbind(obs[obs$occurrence == 1,], abs)
 
   return(obs)
-  
+
 }
