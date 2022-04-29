ebaf5904fa89904c94ca5f4390f591758c3e7d9c
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-04-29 11:21:02 -0400

    Script to make study_extent, mesh, raster and filtered obs

---
+++ analysis/make_study_extent.R
@@ -0,0 +1,68 @@
+# This script create, for every species,: 
+#   - study extent (a polygon around the presence of a species)
+#   - mesh
+#   - raster
+#   - observations filtered with the study extent
+
+#--- Setup ---#
+# Import arguments
+args = commandArgs(trailingOnly=TRUE)
+
+# Import mapSpecies
+suppressMessages(library(raster))
+
+# Set variables
+species <- gsub("output/models/", "", args[1])
+q <- readRDS("data/spacePoly.rds")
+obs <- readRDS(paste0("occurrences/",species,".rds"))
+dist_buffer <- args[2]
+
+# Make directory to store new spatial object
+spat_folder <- paste0("output/spatial/",species,"/")
+dir.create(spat_folder)
+
+# Filter for presence only to define the polygon
+obs_pres <- obs[obs$occurrence == 1,]
+
+# Make the study extent around presence of the species
+xy <- as.data.frame(obs_pres@coords)
+coords.t <- chull(xy[,1], xy[,2])
+xy.bord <- xy[coords.t,]
+xy.bord <- rbind(xy.bord[nrow(xy.bord), ], xy.bord)
+study_extent <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(as.matrix(xy.bord))), 1)))
+study_extent <- sf::st_as_sf(study_extent, crs = raster::crs(obs_pres@proj4string))
+
+if (!is.null(dist_buffer)) {
+  study_extent <-  sf::st_buffer(study_extent, dist =  dist_buffer)
+}
+
+study_extent <- as(study_extent, "Spatial")
+raster::crs(study_extent) <- raster::crs(obs_pres@proj4string)
+
+# Filter observations with study extent
+study_extent <- terra::crop(terra::vect(study_extent), terra::vect(q))
+obs <- terra::crop(terra::vect(obs), study_extent)
+obs <- as(obs, "Spatial")
+study_extent <- as(study_extent, "Spatial")
+raster::crs(study_extent) <- raster::crs(obs_pres@proj4string)
+raster::crs(obs) <- raster::crs(obs_pres@proj4string)
+
+# Make mesh
+pedge <- 0.025
+edge <- min(c(diff(raster::bbox(q)[1,])*pedge,diff(raster::bbox(q)[2,])*pedge))
+mesh <- INLA::inla.mesh.2d(boundary = study_extent,
+                           max.edge = c(edge, edge*4.5), 
+                           min.angle = 21,
+                           offset = c(edge, edge*2), 
+                           cutoff = edge/2, 
+                           crs = raster::crs(study_extent))
+
+# Crop raster with study extent
+raster <- raster::raster("data/rast.gri")
+raster <- raster::crop(raster, study_extent)
+
+# Save all four objects
+raster::writeRaster(raster, paste0(spat_folder,"rast"))
+saveRDS(mesh, paste0(spat_folder,"mesh.rds"))
+saveRDS(obs, paste0(spat_folder,"obs.rds"))
+saveRDS(study_extent, paste0(spat_folder,"study_extent.rds"))
