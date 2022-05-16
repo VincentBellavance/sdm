c78f66852cdc846c545da2da502f7f1e69639c5d
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-05-16 10:29:07 -0400

    bring back aggregate_obs

---
+++ R/aggregate_obs.R
@@ -0,0 +1,23 @@
+#' Title
+#' 
+#' Description
+#'
+#' @param
+#'
+#' @return
+
+aggregate_obs <- function(obs, years, i, rast) {
+
+  # Rasterize to aggregate by cells
+  obs_aggr <- terra::rasterize(terra::vect(obs[obs$year_obs %in% (years+i),]), terra::rast(rast), field = "occurrence", fun = "max")
+
+  # From raster to spatialPoints
+  obs_aggr <- as(terra::as.points(obs_aggr, spatial = TRUE), "Spatial")
+  names(obs_aggr) <- "occurrence"
+
+  # Not sure it's necessary anymore...
+  #obs_aggr <- sp::SpatialPointsDataFrame(obs_aggr[,1:2], as.data.frame(obs_aggr[,3]), proj4string = obs@proj4string)
+  
+  return(obs_aggr)
+
+}
