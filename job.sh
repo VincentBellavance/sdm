22550ecd42cef27dc37b202e8e9299440b69bd37
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-06-08 11:20:41 -0700

    update job.sh

--- job.sh
+++ job.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #SBATCH --account=def-dgravel
-#SBATCH --mem=40GB
+#SBATCH --mem=30GB
 #SBATCH --time=04:00:00
 #SBATCH --mail-user=vincent.bellavance@usherbrooke.ca
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=GAM_all_species
+#SBATCH --job-name=run_sdm
+#SBATCH --output=out/%a.out
 #SBATCH --array=1-196
 
-module load gcc/9.3.0 gdal/3.2.3 r/4.1.2
-export R_LIBS=~/R/x86_64-pc-linux-gnu-library/4.1
+module use /home/belv1601/.local/easybuild/modules/2020/avx2/Compiler/gcc9/
+module load StdEnv/2020  gcc/9.3.0 r-inla/21.05.02 geos/3.9.1 gdal/3.0.4 proj/7.0.1 udunits
 sp=$(cut -d' ' "-f${SLURM_ARRAY_TASK_ID}" <<<cat data/species_vect.txt)
-make models_gam species=$sp
-make maps_gam species=$sp
+make out_dir
+make spatial species=$sp cpu_task=1
+make models species=$sp cpu_task=1
+make maps species=$sp cpu_task=1
+make checks species=$sp
