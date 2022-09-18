2821cf8c88a6d60cee7b6cb234dade44a41fad55
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-09-18 18:20:09 -0400

    rangemap doesn't works on compute canada

--- analysis/make_maps.R
+++ analysis/make_maps.R
@@ -7,10 +7,8 @@ args <- commandArgs(trailingOnly=TRUE)
 suppressMessages(library(raster))
 suppressMessages(library(INLA))
 suppressMessages(library(terra))
-suppressMessages(library(rangemap))
 
 species <- args[1]
-proj <- args[2]
 
 source("R/path.R")
 
@@ -19,18 +17,13 @@ study_extent <- readRDS(paste0(path_sp(species)$spat, "/study_extent.rds"))
 rast <- raster::raster(paste0(path_sp(species)$spat, "/rast.gri")) # Raster pour aggréger les données par cellule
 qc <- readRDS("data/qc_spacePoly.rds")
 mesh <- readRDS(paste0(path_sp(species)$spat, "/mesh.rds"))
-rast_qc <- terra::crop(terra::mask(terra::rast(rast),
-                                   terra::vect(qc)),
-                       terra::vect(qc))
-rast_qc <- raster::raster(rast_qc)
-values(rast_qc)[values(rast_qc) == 1] <- 0
+org <- raster::raster("data/rast.gri")
 
 # List all models from the specific species
 models <- list.files(path_sp(species)$mod)
 
 # List years of the models
 years <- as.integer(gsub(".rds", "", models))
-
 
 for(year in years) {
     
@@ -80,71 +73,13 @@ for(year in years) {
                              crs = mesh$crs)
 
 
-  #---------- Crop les maps avec study_extent ----------#
+  #---------- Project Raster pour être sur le même origin ----------#
 
-  map <- terra::crop(terra::mask(terra::rast(map), 
-                                 terra::vect(study_extent)), 
-                     terra::vect(study_extent))
-  map025 <- terra::crop(terra::mask(terra::rast(map025), 
-                                    terra::vect(study_extent)), 
-                        terra::vect(study_extent))
-  map975 <- terra::crop(terra::mask(terra::rast(map975), 
-                                    terra::vect(study_extent)), 
-                        terra::vect(study_extent))
-
-  ## Reconvertir en raster
-  map_pocc <- raster::raster(map)
-  names(map_pocc) <- paste0(year)
-  map025 <- raster::raster(map025)
-  names(map025) <- paste0(year)
-  map975 <- raster::raster(map975)
-  names(map975) <- paste0(year)
-
-
-  #---------- Binariser les maps ----------#
-
-  # Est-ce que la limite inférieure inclut 0?
-  
-  map <- map_pocc
-  map[map025<= 0.01] <- 0
-  map[map>0] <- 1
-  xy <- raster::coordinates(map)[map[,,] == 1,]
-  xy <- as.data.frame(xy[!is.na(xy[,1]),])
-  colnames(xy) <- c("Longitude", "Latitude")
-  coordinates(xy) <- c("Longitude", "Latitude")
-  proj4string(xy) <- raster::crs(map)@projargs
-  xy <- sp::spTransform(xy, sp::CRS("EPSG:4326"))
-  xy <- cbind(
-    data.frame(Species = species),
-    as.data.frame(xy)
-  )
-  rangemap <- rangemap_hull(xy,
-                            hull_type = "concave", 
-                            concave_distance_lim = 350000, 
-                            buffer_distance = 10000,
-                            final_projection = proj,
-                            extent_of_occurrence = TRUE,
-                            area_of_occupancy = TRUE)
-  rangemap_qc <- terra::intersect(
-                   terra::vect(rangemap@species_range),
-                   terra::vect(qc))
-  rangemap_qc <- as(rangemap_qc, "Spatial")
-  map <- raster::crop(
-    raster::rasterize(raster::aggregate(rangemap_qc),
-                      rast, 
-                      mask = TRUE),
-    rangemap_qc)
-  map <- raster::merge(map, rast_qc)
-  names(map) <- paste0(year)
+  map_pocc <- raster::projectRaster(map_pocc, org)
+  map025 <- raster::projectRaster(map025, org)
+  map975 <- raster::projectRaster(map975, org)
 
   #---------- Stack et sauvegarder les maps ----------#
-
-  ## Stack des cartes de chaque année ensemble
-  if(exists("sdms")) {
-    sdms <- raster::stack(sdms, map)
-  } else {
-    sdms <- raster::stack(map)
-  }
 
   ## Stack des cartes avec pocc de chaque année ensemble
   if(exists("sdms_pocc")) {
@@ -167,8 +102,6 @@ for(year in years) {
 
   ## Si c'est la dernière année, sauvegarder le stack
   if(year == 2018) {
-    raster::writeRaster(sdms, 
-                        paste0(path_sp(species)$maps, "/maps_binary"))
     raster::writeRaster(sdms_pocc, 
                         paste0(path_sp(species)$maps, "maps_pocc"))     
     raster::writeRaster(sdms025, 
