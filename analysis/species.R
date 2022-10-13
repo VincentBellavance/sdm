22e4be5a064287243623f1c82ab5754d54532763
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-10-13 15:12:11 -0400

    simplify species filtering

--- analysis/species.R
+++ analysis/species.R
@@ -1,123 +1,67 @@
-# Get species names (accepted and synonym)
+# Get species names
 
 #--------------------------------------------------------------
 # Steps:
+#   1. Import list of Quebec species (from QuébecOiseaux)
+#   2. Keep Quebec breeding species
+#   3. List family to remove (to remove marine species) and remove them from the list fo Quebec species
+#   4. Remove northern nesting species
+#   5. Validate taxonomy with Atlas taxonomy
 #--------------------------------------------------------------
 
-# Query species for three different sources
-gbif <- ratlas::list_bird_taxa(rank = "species", source_name = "GBIF Backbone Taxonomy") |>
-          (\(.) as.data.frame(.))()
-col <- ratlas::list_bird_taxa(rank = "species", source_name = "Catalogue of Life") |>
-          (\(.) as.data.frame(.))()
+# Import list of Quebec species
+breeding <- read.csv2("data/list_sp_qc.csv")
+breeding <- breeding[breeding$status == "Nicheur", c("species", "family")]
 
 
-# Species that are breeeding in Qc
-breeding <- read.csv2("data/list_sp_qc.csv") |>
-              {\(x) x[x$status == "Nicheur", "species", drop = FALSE]}()
+# Remove family with marine species
+marine_family <- c("anatidae",
+                   "podicipedidae",
+                   "stercorariidae",
+                   "alcidae",
+                   "laridae",
+                   "gaviidae",
+                   "hydrobatidae",
+                   "sulidae",
+                   "phalacrocoracidae")
+keep_family <- c("rallidae",
+                 "charadriidae",
+                 "ardeidae")
 
-valid_breeding <- ratlas::get_taxa(scientific_name = breeding$species)
-
-tmp <- dplyr::left_join(breeding, valid_breeding[,c("observed_scientific_name", "valid_scientific_name")], by = c(species = "observed_scientific_name"))
-
-# Note which species are not a breeding
-removed_species <- unique(col[col$valid_srid %in% col[!col$scientific_name %in% tmp$valid_scientific_name, "valid_srid"], "scientific_name"],
-                          gbif[gbif$valid_srid %in% gbif[!gbif$scientific_name %in% tmp$valid_scientific_name, "valid_srid"], "scientific_name"])
-line <- paste0(paste0(removed_species, ";Is not a Qc breeding bird"), collapse = "\n")
-write(line, file = "removed_species.csv", append = T)
-
-# Filter species
-col <- col[col$valid_srid %in% col[col$scientific_name %in% tmp$valid_scientific_name, "valid_srid"],]
-gbif <- gbif[gbif$valid_srid %in% gbif[gbif$scientific_name %in% tmp$valid_scientific_name, "valid_srid"],]
+breeding <- breeding[!breeding$family %in% marine_family, "species"]
 
 
-# Marine species
-marine <- read.csv2("data/list_marine_sp.csv")
-
-valid_marine <- ratlas::get_taxa(scientific_name = marine$species)
-
-tmp <- dplyr::left_join(marine, valid_marine[,c("observed_scientific_name", "valid_scientific_name")], by = c(species = "observed_scientific_name"))
-
-# Note which species are marine (therefore not modelled)
-removed_species <- unique(col[col$valid_srid %in% col[col$scientific_name %in% tmp$valid_scientific_name, "valid_srid"], "scientific_name"],
-                          gbif[gbif$valid_srid %in% gbif[gbif$scientific_name %in% tmp$valid_scientific_name, "valid_srid"], "scientific_name"])
-line <- paste0(paste0(removed_species, ";Is a marine species"), collapse = "\n")
-write(line, file = "removed_species.csv", append = T)
-
-# Filter species
-gbif <- gbif[!gbif$valid_srid %in% gbif[gbif$scientific_name %in% tmp$valid_scientific_name, "valid_srid"],]
-col <- col[!col$valid_srid %in% col[col$scientific_name %in% tmp$valid_scientific_name, "valid_srid"],]
+# Remove northern nesting species
+north_nest <- read.csv2("data/list_sp_qc_winter.csv")
+north_nest <- north_nest[grep("\\(N\\)", north_nest$status), "species"]
 
 
-# Minimum observations necessary for every species
-avg_limit <- 30
-tot_limit <- 300
-
-# Note which species have been removed
-removed_species <- unique(col[col$avg_yearly_count < avg_limit & col$total_count < tot_limit, "scientific_name"],
-                          gbif[gbif$avg_yearly_count < avg_limit & gbif$total_count < tot_limit, "scientific_name"])
-line <- paste0(paste0(removed_species, ";Not enough data"), collapse = "\n")
-write(line, file = "removed_species.csv", append = T)
-
-# Filter species
-col <- col[col$avg_yearly_count >= avg_limit & col$total_count >= tot_limit, ]
-gbif <- gbif[gbif$avg_yearly_count >= avg_limit & gbif$total_count >= tot_limit, ]
-
-
-# Make list
-list_ref <- lapply(unique(col$valid_srid), function(x) {
-  
-  accepted <- col[col$valid_srid == x &
-                  col$valid, "scientific_name"]
-  
-  tmp <- list(
-    accepted = accepted
-  )
-
-  return(tmp)
-
+# Validate names in breeding and north_west to compare with the same names
+valid_breeding <- lapply(breeding, function(x) {
+  tmp <- ratlas::get_taxa(scientific_name = x)
+  return(unique(tmp[tmp$rank == "species", "valid_scientific_name"]))
 })
-
-
-# Synonym in GBIF that are accepted in COL
-accepted_col <- unlist(lapply(list_ref, "[[", 1))
-
-for(i in unique(gbif[!gbif$valid, "scientific_name"])) {
-  if(i %in% accepted_col) {
-    valid_srid <- gbif[gbif$scientific_name == i, "valid_srid"]
-    syn_gbif <- gbif[gbif$valid_srid == valid_srid & gbif$valid, "scientific_name"]
-    list_ref[[which(accepted_col %in% i)]][["synonym"]] <- unique(c(syn_gbif, list_ref[[which(accepted_col %in% i)]][["synonym"]]))
-  }
-}
-
-# Accepted GBIF that are not in COL
-add_gbif <- gbif[!gbif$scientific_name %in% unlist(list_ref) & gbif$valid, 
-                        c("scientific_name", "valid_srid", "valid", "source_record_id")]
-
-gbif_list <- lapply(unique(add_gbif$valid_srid), function(x) {
-  
-  accepted <- gbif[gbif$valid_srid == x &
-                  gbif$valid, "scientific_name"]
-  synonym <- gbif[gbif$valid_srid == x & 
-                 !gbif$valid, "scientific_name"]
-  
-  tmp <- list(
-    accepted = accepted
-  )
-
-  if(length(synonym) > 0) {
-    tmp[["synonym"]] <- synonym
-  }
-
-  return(tmp)
-
+valid_north_nest <- lapply(north_nest, function(x) {
+  tmp <- ratlas::get_taxa(scientific_name = x)
+  return(unique(tmp[tmp$rank == "species", "valid_scientific_name"]))
 })
+breeding <- as.vector(unlist(valid_breeding))
+north_nest <- as.vector(unlist(valid_north_nest))
 
-final_list <- c(list_ref, gbif_list)
 
-#final_list <- list(list(accepted = "Grus canadensis", synonym = "Antigone canadensis"), list(accepted = "Setophaga petechia"))
+# Final list of species
+final_list <- breeding[!breeding %in% north_nest]
 
-lapply(final_list, function(x) {
-  cat(paste0(tolower(gsub(" ", "_", x$accepted))," "), file = "data/species_vect.txt", append = TRUE)
+sapply(final_list, function(x) {
+  if(x == final_list[length(final_list)]) {
+    cat(tolower(gsub(" ", "_", x)), 
+        file = "data/species_vect.txt", 
+        append = TRUE)
+  } else {
+    cat(paste0(tolower(gsub(" ", "_", x))," "), 
+        file = "data/species_vect.txt", 
+        append = TRUE)
+  }
 })
 
 saveRDS(final_list, "data/species.rds")
