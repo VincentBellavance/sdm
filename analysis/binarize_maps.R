260ce13c2a9f2af0af132fb3186809b2a5163825
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-12-02 12:50:18 -0500

    use mask_keep_partial function

--- analysis/binarize_maps.R
+++ analysis/binarize_maps.R
@@ -6,20 +6,21 @@ library(raster)
 library(INLA)
 library(ggplot2)
 library(rangemap)
+library(dismo)
 
 args <- commandArgs(trailingOnly=TRUE)
 species <- args[1]
 zone <- args[2]
-threshold <- as.numeric(args[3])
+sensitivity <- as.numeric(args[3])
+years <- 1992:2018
 
+source("R/find_threshold.R")
+source("R/mask_keep_partial.R")
 source("R/path.R")
 
 org <- raster::raster("data/rast.gri")
 qc <- readRDS(paste0("data/",zone,"/qc_spacePoly.rds"))
-rast_qc <- terra::crop(terra::mask(terra::rast(org),
-                                   terra::vect(qc)),
-                       terra::vect(qc))
-rast_qc <- raster::raster(rast_qc)
+rast_qc <- mask_keep_partial(org, qc)
 values(rast_qc)[values(rast_qc) == 1] <- 0
 
 # Import spatial opbjects for the species
@@ -27,99 +28,23 @@ study_extent <- readRDS(paste0(path_sp(species, zone)$spat, "/study_extent.rds")
 rast <- raster::raster(paste0(path_sp(species, zone)$spat, "/rast.gri"))
 qc <- readRDS(paste0("data/",zone,"/qc_spacePoly.rds"))
 mesh <- readRDS(paste0(path_sp(species, zone)$spat, "/mesh.rds"))
+obs_all <- readRDS(paste0(path_sp(species, zone)$spat, "/obs.rds"))
 
-# List models and stacks
-models <- list.files(path_sp(species, zone)$mod)
-Stacks <- list.files(path_sp(species, zone)$stack)
-years <- as.integer(gsub(".rds", "", models))
-
+# maps of 2.5e quantile
+maps025 <- stack(paste0(path_sp(species, zone)$maps, "/maps_025.gri"))
 
 #---------- CREATE BINARY MAP FOR EVERY YEAR ----------#
 
-binary_maps <- lapply(1:length(models), function(x) {
-
-  cat(paste0("Sampling for year ",years[x]," in progress : ",x,"/",length(models)), "\n")
-
-  #---------- POSTERIOR SAMPLINGS ----------#
-  
-  # Import model
-  mod <- readRDS(paste0(path_sp(species, zone)$mod, "/", models[x]))
-  Stack <- readRDS(paste0(path_sp(species, zone)$stack, "/", Stacks[x]))
-
-
-  # Make map for entire sPoly to compute AUC
-  # mapBasis
-  mapBasis <- inla.mesh.projector(mesh,
-                                  dims = dim(rast)[2:1],
-                                  xlim = c(xmin(rast), 
-                                           xmax(rast)),
-                                  ylim = c(ymin(rast), 
-                                           ymax(rast)),
-                                  crs = mesh$crs)
-
-  ### Find the mesh edges on which predictions should be made
-  ID <- inla.stack.index(Stack, tag="pred")$data
-  pred <- suppressMessages(
-            inla.posterior.sample(1000, 
-                                  result = mod, 
-                                  selection = list(APredictor = 0,
-                                                   Predictor = 0),
-                                  intern = TRUE,
-                                  use.improved.mean = FALSE))
-
-  maps_list <- lapply(1:length(pred), function(y) {
-    
-    fitted <- exp(pred[[y]]$latent[ID,])/(1 + exp(pred[[y]]$latent[ID,]))
-    mapPred <- inla.mesh.project(mapBasis, fitted)
-    mapRaster <- raster::raster(t(mapPred[,ncol(mapPred):1]),
-                                  xmn = min(mapBasis$x), 
-                                  xmx = max(mapBasis$x), 
-                                  ymn = min(mapBasis$y), 
-                                  ymx = max(mapBasis$y),
-                                  crs = mesh$crs)
-    names(mapRaster) <- paste0(years[x])
-    cat("\r", paste0("... Iteration ",y," of ",length(pred)," done!"))
-    if(y == length(pred)) cat("\n")
-    return(mapRaster)
-  })      
-    
-  maps <- do.call(raster::stack, maps_list)
-
-
-  #---------- BINARIZE MAPS ----------#
-
-  maps_df <- maps[1:raster::ncell(maps)]
-
-  binary_values <- sapply(1:nrow(maps_df), function(i) {
-    distr <- maps_df[i,]
-    if(all(is.na(distr))) return(NA)
-    quant <- quantile(distr, probs=0.025, na.rm = TRUE)
-    if(quant<threshold) {
-      return(0)
-    } else {
-      return(1)
-    }
-  })
-
-  binary_map <- raster(crs = raster::crs(maps),
-                       ext = raster::extent(maps),
-                       resolution = round(raster::res(maps)),
-                       vals = binary_values)
-
-  return(binary_map)
-
-  cat(paste0("Sampling for year ",years[x]," done! : ",x,"/",length(models)), "\n")
+binary_maps <- lapply(1:length(names(maps025)), function(x) {
+  obs <- obs_all[obs_all$year_obs %in% (years[x]-2):(years[x]+2),]
+  sdm <- maps025[[x]]
+  threshold <- find_threshold(sdm, obs, type = "sensitivity", sensitivity)
+  sdm[sdm >= threshold] <- 1
+  sdm[sdm < threshold] <- 0
+  return(sdm)
 })
-
-# Gather maps in a rasterStack and crop it with Qc sPoly
 binary_maps <- do.call(raster::stack, binary_maps)
-names(binary_maps) <- years
-
-binary_maps_final <- terra::crop(
-                       terra::mask(terra::rast(binary_maps), 
-                                   terra::vect(qc)),
-                       terra::vect(qc))
-binary_maps_final <- raster::stack(binary_maps_final)
+binary_maps_final <- mask_keep_partial(binary_maps, qc)
 binary_maps_final <- raster::merge(binary_maps_final, rast_qc, overlap = FALSE)
 names(binary_maps_final) <- years
 
@@ -132,11 +57,8 @@ raster::writeRaster(binary_maps_final,
 
 #---------- MAKE MAPS OF SPECIES RANGE ----------#
 
-# Crop and mask the rasters with study_extent to make rangemaps 
-binary_maps <- raster::crop(
-                raster::mask(binary_maps, 
-                             study_extent),
-                qc)
+# Crop and mask the rasters with study_extent to make rangemaps
+binary_maps <- mask_keep_partial(binary_maps, study_extent)
 
 for(i in 1:length(names(binary_maps))) {
     
@@ -190,26 +112,13 @@ for(i in 1:length(names(binary_maps))) {
                               extent_of_occurrence = FALSE,
                               area_of_occupancy = FALSE)
 
-    rangemap_qc <- terra::intersect(
-                     terra::vect(terra::aggregate(rangemap@species_range)),
-                     terra::vect(qc))
-
     # If the range intersect with the Quebec polygon
     if(terra::geomtype(rangemap_qc) != "none") {
-      rangemap_qc <- as(rangemap_qc, "Spatial")
-
-      rangemap_qc <- raster::mask(
-                       raster::crop(
-                         raster::rasterize(rangemap_qc,
-                                           rast_qc, 
-                                           mask = TRUE),
-                         rangemap_qc),
-                       qc)
-      rangemap_qc[rangemap_qc == 0] <- 1
-      rangemap_qc[is.na(rangemap_qc)] <- 0
-
-      rangemap_qc <- raster::merge(rangemap_qc, rast_qc, overlap = FALSE)
-      rangemap_qc <- raster::mask(rangemap_qc, qc)
+      rangemap_qc <- raster::rasterize(rangemap@species_range,
+                                       rast_qc,
+                                       getCover = TRUE)
+      rangemap_qc[rangemap_qc > 0] <- 1
+      rangemap_qc <- raster::mask(rangemap_qc, rast_qc)
       names(rangemap_qc) <- paste0(c(1992:2018)[i])
 
       if(exists("sdms_range")) {
@@ -239,9 +148,8 @@ for(i in 1:length(names(binary_maps))) {
 
     # If there is not enough points to make a range polygon
 
-    rangemap_qc <- raster::crop(raster::mask(map, qc),qc)
-    rangemap_qc <- raster::merge(rangemap_qc, rast_qc, overlap = FALSE)
-    rangemap_qc <- raster::mask(rangemap_qc, qc)
+    rangemap_qc <- mask_keep_partial(map, qc)
+    rangemap_qc <- raster::merge(rangemap_qc, rast_qc, overlap = TRUE)
 
     if(exists("sdms_range")) {
       sdms_range <- raster::stack(sdms_range, rangemap_qc)
