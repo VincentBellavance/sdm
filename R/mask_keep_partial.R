3d4a6b942cd99324a71037a8790c61c51135d2b1
Vincent Bellavance <vincent.bellavance@usherbrooke.ca>
2022-12-08 23:09:47 -0500

    fix y args in crop fct in the return()

--- R/mask_keep_partial.R
+++ R/mask_keep_partial.R
@@ -7,7 +7,7 @@
 #'
 #' @return Raster
 
-mask_keep_partial <- function(x, y) {
+mask_keep_partial <- function(x, y, qc) {
 
   if(class(x) == "RasterStack" | class(x) == "RasterBrick") {
     spoly_rast <- raster::rasterize(y, x[[1]], getCover = TRUE)
@@ -16,6 +16,6 @@ mask_keep_partial <- function(x, y) {
   }
   spoly_rast[spoly_rast == 0] <- NA
   masked_rast <- mask(x, spoly_rast)
-  return(crop(masked_rast, qc, snap = "out"))
+  return(crop(masked_rast, y, snap = "out"))
 
 }
